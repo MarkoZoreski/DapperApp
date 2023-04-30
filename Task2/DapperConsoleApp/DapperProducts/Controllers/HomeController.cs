19933acee2aa212253ca0e8a4ac35ede903fbc80
MarkoZoreski <markozoreski@gmail.com>
2023-04-30 15:42:26 +0200

    added mvc products page

---
+++ Task2/DapperConsoleApp/DapperProducts/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using DapperConsoleApp.Repository;
+using DapperProducts.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperProducts.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productRepository.GetAllProducts();
+            return View(products);
+        }
+
+    }
+}
